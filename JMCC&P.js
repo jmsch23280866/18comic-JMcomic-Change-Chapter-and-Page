30528e4305525e1b16ae3d6fc51f037911387796
特務E04 <jmsch23280866@gmail.com>
2024-09-04 22:19:05 +0800

    Update JMCC&P.js

--- JMCC&P.js
+++ JMCC&P.js
@@ -3,7 +3,7 @@
 // @name:zh-TW   禁漫天堂-快速切換上下話與頁面
 // @name:zh-CN   禁漫天堂-快速切换上下话与页面
 // @namespace    https://github.com/jmsch23280866
-// @version      1.0
+// @version      1.0.1
 // @description        使用 Shift + ( ← 或 A ) 切換上一話，Shift + ( → 或 D ) 切換下一話。使用 [←] 或 [A] 切換上一頁，使用 [→] 或 [D] 切換下一頁。(此腳本由ChatGPT協助撰寫)
 // @description:zh-TW  使用 Shift + ( ← 或 A ) 切換上一話，Shift + ( → 或 D ) 切換下一話。使用 [←] 或 [A] 切換上一頁，使用 [→] 或 [D] 切換下一頁。(此腳本由ChatGPT協助撰寫)
 // @description:zh-CN  使用 Shift + ( ← 或 A ) 切换上一话，Shift + ( → 或 D ) 切换下一话。使用 [←] 或 [A] 切换上一页，使用 [→] 或 [D] 切换下一页。(此脚本由ChatGPT协助撰写)
@@ -101,7 +101,7 @@
         });
         setTimeout(() => {
             isScrolling = false;
-        }, 500); // 等待滾動完成後重置滾動狀態
+        }, 350); // 等待滾動完成後重置滾動狀態
     };
 
     // 停止滾動
